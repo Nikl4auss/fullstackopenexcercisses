0de2be561228793c6c91222fb120559aced7d4ee
Niklauss <nicolasmarinelli@protonmail.com>
2021-02-19 17:55:02 -0300

    Finished the file structure and separation of functionality

---
+++ part4/bloglist/app.js
@@ -0,0 +1,40 @@
+const config = require("./utils/config");
+const express = require("express");
+const blogsRouter = require("./controllers/blogs");
+const middleware = require("./utils/middleware");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const Blog = require("./modules/blog");
+
+console.log(config.MONGODB_URI);
+console.log(config.PORT);
+
+mongoose
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("connected to mongoDB");
+  })
+  .catch((error) => {
+    console.error("error connecting to MongoDB", error.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World</h1>");
+});
+
+app.use("/api/blogs", blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+
+app.use(middleware.errorHandler);
+
+module.exports = app;
--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,42 +1,9 @@
+const app = require("./app");
+const http = require("http");
 const config = require("./utils/config");
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
-const Blog = require("./modules/blog");
 
-console.log(config.MONGODB_URI);
-console.log(config.PORT);
+const server = http.createServer(app);
 
-mongoose.connect(config.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-});
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World</h1>");
-});
-
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
-
-const PORT = config.PORT;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(config.PORT, () => {
+  console.log(`connected to port ${config.PORT}`);
 });
